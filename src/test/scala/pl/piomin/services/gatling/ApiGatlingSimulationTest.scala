698350e3267c4dd841801572f23c2d9e2f96532a
piotr.minkowski <piotr.minkowski@gmail.com>
2018-01-18 16:04:06 +0100

    changing gatling scenario

--- src/test/scala/pl/piomin/services/gatling/ApiGatlingSimulationTest.scala
+++ src/test/scala/pl/piomin/services/gatling/ApiGatlingSimulationTest.scala
@@ -12,15 +12,15 @@ import java.util.concurrent.TimeUnit
 
 class ApiGatlingSimulationTest extends Simulation {
 
-  val scn = scenario("AddPersons").repeat(2000, "n") {
+  val scn = scenario("AddAndFindPersons").repeat(1000, "n") {
         exec(
           http("AddPerson-API")
             .post("http://localhost:8090/persons")
             .header("Content-Type", "application/json")
-            .body(StringBody("""{"firstName":"John${n}","lastName":"Smith${n}","birthDate":"1980-01-01"}"""))
+            .body(StringBody("""{"firstName":"John${n}","lastName":"Smith${n}","birthDate":"1980-01-01", "address": {"country":"pl","city":"Warsaw","street":"Test${n}","postalCode":"02-200","houseNo":${n}}}"""))
             .check(status.is(200))
-        ).pause(Duration.apply(10, TimeUnit.MILLISECONDS))    
-  }.repeat(2000, "n") {
+        ).pause(Duration.apply(5, TimeUnit.MILLISECONDS))    
+  }.repeat(1000, "n") {
         exec(
           http("GetPerson-API")
             .get("http://localhost:8090/persons/${n}")
@@ -28,6 +28,6 @@ class ApiGatlingSimulationTest extends Simulation {
         )   
   }
   
-  setUp(scn.inject(atOnceUsers(50))).maxDuration(FiniteDuration.apply(10, "minutes"))
+  setUp(scn.inject(atOnceUsers(30))).maxDuration(FiniteDuration.apply(10, "minutes"))
   
 }
